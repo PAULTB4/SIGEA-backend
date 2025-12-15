79746c9daa5f385b2035abaf2316f9b204dd6806
Dxr0n <186491058+Dxr0n@users.noreply.github.com>
2025-12-14 19:33:44 -0500

    Para el entorno en la nube

--- src/main/java/com/zentry/sigea/SigeaApplication.java
+++ src/main/java/com/zentry/sigea/SigeaApplication.java
@@ -8,16 +8,19 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling // Habilita las tareas cada cierto tiempo
+
 public class SigeaApplication {
+public static void main(String[] args) {
+        // Cargar .env solo si existe (Local), si no, ignorar (Nube)
+        try {
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+            dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+            );
+        } catch (Exception e) {
+            System.out.println("Archivo .env no encontrado. Usando variables de entorno del sistema.");
+        }
 
-	public static void main(String[] args) {
-		// Cargar variables en .env antes de iniciar la app
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-
-		SpringApplication.run(SigeaApplication.class, args);
-	}
-
+        SpringApplication.run(SigeaApplication.class, args);
+    }
 }
